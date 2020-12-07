3c99ca820648fa591ea0e4f6fe28e9f051c9920c
Fujinjakka <calvieri.marco@gmail.com>
2020-12-07 14:09:42 +0100

    complete

--- script.js
+++ script.js
@@ -18,3 +18,199 @@
 // Popolare le options della select dinamicamente
 // e, ogni volta che cambia il valore selezionato,
 // visualizzare le icone corrispondenti.
+
+
+$(document).ready(
+  function(){
+
+    const icons = [
+      {
+        name: "dog",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "fish",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "tractor",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "hippo",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "spider",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "apple-alt",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "carrot",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "lemon",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "dragon",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "pepper-hot",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "space-shuttle",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "truck-monster",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "wheelchair",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "tram",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "fighter-jet",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "helicopter",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+    ];
+
+    const colors = [
+      "red",
+      "blue",
+      "green"
+    ];
+
+    // {name: "dog",
+    // prefix: "fa-",
+    // type: "animal",
+    // family: "fas"}
+
+    const container = $(".icone");
+
+    const tipo = tipoColore(icons);
+
+    const iconeColorate = copiaColore(icons, colors, tipo);
+
+    stampa(iconeColorate, container);
+
+    selezionaOpzione($("#type"), tipo);
+
+    $("#type").change(
+      function(){
+        const tipoSelzionato = $(this).val();
+        if (tipoSelzionato == ""){
+          stampa(iconeColorate, container);
+        } else {
+          const iconeFiltrate = iconeColorate.filter(
+            (element) => {
+              return element.type == tipoSelzionato;
+            }
+          );
+          stampa(iconeFiltrate, container);
+        }
+      }
+    );
+
+
+
+  }
+);
+
+//-------------------------------
+// FUNCTIONS
+
+function stampa(array, container){
+  container.html("");
+  array.forEach((element, i) => {
+    const {name, prefix, family, color} = element;
+    container.append(`
+      <div>
+        <i class="${family} ${prefix}${name}" style="color:${color}"></i>
+        <div>${name.toUpperCase()}</div>
+      </div>
+    `);
+  });
+}
+
+function tipoColore(array){
+  const tipo = [];
+
+  array.forEach((element) => {
+    if (tipo.includes(element.type) == false) {
+      tipo.push(element.type);
+    }
+  });
+  return tipo;
+}
+
+function copiaColore(iconeArray, coloreArray, tipoArray) {
+
+  const newArray = iconeArray.map(
+    (element) => {
+      const posizioneTipo = tipoArray.indexOf(element.type);
+      const iconeColorate = coloreArray[posizioneTipo];
+      const newElement = {
+        ...element,
+        color: iconeColorate
+      };
+      return newElement;
+    }
+  );
+  return newArray;
+}
+
+function selezionaOpzione(select, tipoArray) {
+  tipoArray.forEach((element) => {
+    select.append(`
+      <option value="${element}">${element}</option>
+      `);
+  });
+}
